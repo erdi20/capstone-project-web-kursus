f3f8e788577f1903b580f942a43e21595c0a0f59
Nurul-1006 <ajwidatun@gmail.com>
2025-11-23 06:55:16 +0700

    membuat view quiz, haisl quiz, esay

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AssignmentsController;
-use App\Http\Controllers\ClassStudentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,15 +15,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/course', function () {
         return view('student.course');
     });
-    Route::get('/kelas', [ClassStudentController::class, 'show'])->name('course.show');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/hasilQuiz', [AssignmentsController::class, 'hasilQuiz'])->name('course.hasilQuiz');
-    Route::get('/esay', [AssignmentsController::class, 'essay'])->name('course.essay');
-    Route::get('/quiz', [AssignmentsController::class, 'quiz'])->name('course.quiz');
-    Route::POST('/submitQuiz', [AssignmentsController::class, 'submit'])->name('course.submitQuiz');
-    Route::POST('/submitEssay', [AssignmentsController::class, 'submitEssay'])->name('course.submitEssay');
+    Route::view('/kelas', 'student.class');
+    Route::view('/quiz', 'student.quiz');
+    Route::view('/hasilQuiz', 'student.hasilQuiz');
+    Route::view('/esay', 'student.essay');
 });
 
 require __DIR__ . '/auth.php';
