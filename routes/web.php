69f5eadf85aa574f8136fb7a2f883b749480386e
Nurul-1006 <ajwidatun@gmail.com>
2025-11-22 21:34:12 +0700

    view quiz (selesai), hasil quiz (kurang), essay (blm bangeetttt)

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/course', function () {
         return view('student.course');
     });
-    Route::get('/kelas', [ClassController::class, 'show'])->name('course.show');
+    Route::get('/kelas', [ClassStudentController::class, 'show'])->name('course.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
